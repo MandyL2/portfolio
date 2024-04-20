9f782f841eae2f7d8045e5f4dd52d504e77308d1
MandyL2 <fioleewriter@gmail.com>
2024-04-20 19:25:36 -0400

    portfolio revamp

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ const options = {
 		"b-embed": "src",
 	},
 };
+
+// var string = '<div> <h1> Mandy Luu </h1> <p> Portfolio site. ©2024 Mandy L. - All Rights Reserved </p>' +
+// 					'<p> <a href="/indexMain.html"> Home </a> <a href="/about.html"> About </a> Art <a href="/games.html">' +
+// 					'Games </a> <a href="/programming.html"> Programming </a> </p> </div> <div> <p> <br /> Get in touch!' +
+// 					'Email manyisart@gmail.com and follow @man_yisart on Twitter for regular updates. </p> <div> ' +
+// 					'<p class="h2"> <a href="https://www.linkedin.com/in/luumandy/" target="_blank"> <b-icon-linkedin ' +
+// 					'variant="danger"></b-icon-controller> </a> <a href="https://github.com/mandyL2" target="_blank">' +
+// 					'<b-icon-github variant="danger"></b-icon-github> </a> <a href="https://twitter.com/man_yisArt" ' +
+// 					'target="_blank"> <b-icon-twitter variant="danger"></b-icon-twitter> </a> ' +
+// 					'<a href="https://youtube.com/@manyi5427?si=n6xYA2t91CPLNcyw" target="_blank"> <b-icon-youtube ' +
+// 					'variant="danger"></b-icon-youtube> </a> <a href="https://mandyl2.itch.io/" target="_blank">' +
+// 					'<b-icon-controller variant="danger"></b-icon-controller> </a> </p> </div> </div>';
+// 			document.getElementById('foot').innerHTML += string;
